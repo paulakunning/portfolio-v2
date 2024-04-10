986300de52ae7d3e3f2489c0f7221069b4ed30ee
Paula Kunning <paulakunning@gmail.com>
2024-04-09 23:33:24 -0300

    switch language config added :sparkles:

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-function App() {
-  
+import React from 'react';
+import i18n from './i18n';
+import { withNamespaces } from 'react-i18next';
+
+function App ({t}) {
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  }
 
   return (
-    <>
-      
-      <h1>Portfolio</h1>
-     
-     
-    </>
+    <div>
+      <button onClick={() => changeLanguage('en')}>EN</button>
+      <button onClick={() => changeLanguage('es')}>ES</button>
+      <h1>{t("home.title")}</h1>
+    </div>
   )
 }
 
-export default App
+export default withNamespaces()(App);
