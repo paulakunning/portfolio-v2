3c455ac119767d6d0f59c2a92e13071009281154
Paula Kunning <paulakunning@gmail.com>
2024-04-13 19:37:12 -0300

    about me section structure and desktop styles :sparkles:

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import i18n from '../../i18n';
+import './About.scss'
 
 
 export default function AboutMe() {
   return (
-    <div>
-        <p>{i18n.t('about.p1')}</p>
-        <p>{i18n.t('about.p2')}</p>
-        <p>{i18n.t('about.p3')}</p>
-        <p>{i18n.t('about.p4')}</p>
+    <div className='about-wrapper'>
+      <div>
+        <h3 className='section-title'>{i18n.t('about.title')}</h3>
+      </div>
+      <div className='about-content'>
+        <div>
+          <img src="src\assets\paulakunning-profilepic.png" alt="" />
+          </div>
+        <div className='text-container'>
+          <p>{i18n.t('about.p1')}</p>
+          <p>{i18n.t('about.p2')}</p>
+          <p>{i18n.t('about.p3')}</p>
+          <p>{i18n.t('about.p4')}</p>
+        </div>
+      </div>
     </div>
   )
 }
