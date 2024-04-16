6b1f13ec6e2d0a2ed15f223f2c7b0292179d6d9c
Paula Kunning <paulakunning@gmail.com>
2024-04-16 11:48:52 -0300

    services section added

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,16 +1,40 @@
 import React from 'react'
 import i18n from '../../i18n'
+import './Services.scss'
 
 
 export default function Services() {
   return (
     <div>
-        <p>{i18n.t('services.s1')}</p>
-        <p>{i18n.t('services.s2')}</p>
-        <p>{i18n.t('services.s3')}</p>
-        <p>{i18n.t('services.s4')}</p>
-        <p>{i18n.t('services.s5')}</p>
-        <p>{i18n.t('services.s6')}</p>
+      <div className='service-cards-container'>
+        <div className='service-card'>
+          <img src="src\assets\custom-web-development.png" alt="" />
+          <p>{i18n.t('services.s1')}</p>
+        </div>
+        <div className='service-card'>
+          <img src="src\assets\full-stack-development.png" alt="" />
+          <p>{i18n.t('services.s2')}</p>
+        </div>
+        <div className='service-card'>
+          <img src="src\assets\wordpress-development.png" alt="" />
+          <p>{i18n.t('services.s3')}</p>
+        </div>
+      </div>
+
+      <div  className='service-cards-container'>
+        <div className='service-card'>
+          <img src="src\assets\seo.png" alt="" />
+          <p>{i18n.t('services.s4')}</p>
+        </div>
+        <div className='service-card'>
+          <img src="src\assets\responsive-design.png" alt="" />
+          <p>{i18n.t('services.s5')}</p>
+        </div>
+        <div className='service-card'>
+          <img src="src\assets\mantenimiento.png" alt="" />
+          <p>{i18n.t('services.s6')}</p>
+        </div>
+      </div>
 
 
     </div>
